445a4b26e10853cf27c5e56041de51a6ac463cef
otherdcn <danny.c.ntwari@gmail.com>
2024-07-09 20:07:43 +0200

    Use CSV to display names and formatted zip-codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,24 @@
+require 'csv'
+
 puts "Event Manager Initialized!"
 
-small_sample_event_attendees_file = "./event_attendees.csv"
-puts "Does small sample data file exist: #{File.exist? small_sample_event_attendees_file}"
+small_attendees_file = "./event_attendees.csv"
+puts "Does small sample data file exist: #{File.exist? small_attendees_file}\n\n"
 
+
+contents = CSV.open(
+  small_attendees_file,
+  headers: true,
+  header_converters: :symbol
+)
+
+def clean_zipcodes(zipcode)
+  zipcode.to_s.rjust(5,'0')[0..4]
+end
+
+contents.each do |row|
+  name = row[:first_name].ljust(15, ' ')
+  zipcode = clean_zipcodes(row[:zipcode])
+
+  puts "Name: #{name} | Zip-code: #{zipcode}"
+end
