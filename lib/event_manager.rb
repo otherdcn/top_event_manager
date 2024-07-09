a40f40cd6b383b131b40c430f24fccbd1e49b25b
otherdcn <danny.c.ntwari@gmail.com>
2024-07-09 19:39:19 +0200

    Download small sample data

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,5 @@
 puts "Event Manager Initialized!"
+
+small_sample_event_attendees_file = "./event_attendees.csv"
+puts "Does small sample data file exist: #{File.exist? small_sample_event_attendees_file}"
+
