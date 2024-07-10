ce0dce99a1ba0c1416cbe18247002e655aedf56f
otherdcn <danny.c.ntwari@gmail.com>
2024-07-10 20:24:43 +0200

    Clean phone numbers to format and ensure validity

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -18,22 +18,47 @@ def clean_zipcodes(zipcode)
 end
 
 def legislators_by_zipcode(zipcode)
- civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
- civic_info.key = File.read('secret.key').strip
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('secret.key').strip
 
- begin
+  begin
     legislators = civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
     ).officials
-
-    #legislators = legislators.officials
-    #legislator_names = legislators.map(&:name)
-    #legislator_names.join(", ")
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
+end
+
+def clean_phone_numbers(num)
+  only_num_strings = "0".upto("9")
+  num_sliced = num.split("")
+
+  num_sliced.select! do |ele|
+    only_num_strings.include? ele
+  end
+
+  final_num = if !(num_sliced.size.between?(10,11))
+                # Bad number: digits too short or too long
+                "N/A"
+              elsif num_sliced.size == 11 && num_sliced.first != "1"
+                # Bad number: should start with 1 if more than 10 digits
+                "N/A"
+              elsif num_sliced.size == 11 && num_sliced.first == "1"
+                # Good number, trim the 1 and use remaining 10 digitis
+                num_sliced.delete_at(0)
+                num_sliced.join
+              elsif num_sliced.size == 10
+                # Good number
+                num_sliced.join
+              else
+                # Bad number: unknown reason
+                "N/A"
+              end
+
+  final_num
 end
 
 def save_thank_you_letter(id, form_letter)
@@ -55,13 +80,11 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]#.ljust(15, ' ')
   zipcode = clean_zipcodes(row[:zipcode])#.ljust(10, ' ')
+  home_phone = clean_phone_numbers(row[:homephone])#.ljust(15, ' ')
 
   legislators = legislators_by_zipcode(zipcode)
 
-  #puts "Name: #{name} | Zip-code: #{zipcode} | #{legislators}"
-
-  #personal_letter = template_letter.gsub('FIRST_NAME', name)
-  #personal_letter.gsub!('LEGISLATORS', legislators)
+  # puts "Name: #{name} | Zip-code: #{zipcode} | Home Phone: #{home_phone}"
 
   form_letter = erb_template.result(binding)
 
