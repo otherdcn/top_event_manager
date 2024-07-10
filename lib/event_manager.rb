ab0a85de967f80a2aeff99d4219c664c3c4ee6c8
otherdcn <danny.c.ntwari@gmail.com>
2024-07-10 17:15:17 +0200

    Form thank you letters to attendees using erb

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 puts "Event Manager Initialized!"
 
@@ -25,23 +26,44 @@ def legislators_by_zipcode(zipcode)
       address: zipcode,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
+    ).officials
 
-    legislators = legislators.officials
-
-    legislator_names = legislators.map(&:name)
-
-    legislator_names.join(", ")
+    #legislators = legislators.officials
+    #legislator_names = legislators.map(&:name)
+    #legislator_names.join(", ")
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+
+  puts "Saved thank you letter for user #{id}"
+end
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
-  name = row[:first_name].ljust(15, ' ')
-  zipcode = clean_zipcodes(row[:zipcode]).ljust(10, ' ')
+  id = row[0]
+  name = row[:first_name]#.ljust(15, ' ')
+  zipcode = clean_zipcodes(row[:zipcode])#.ljust(10, ' ')
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "Name: #{name} | Zip-code: #{zipcode} | #{legislators}"
+  #puts "Name: #{name} | Zip-code: #{zipcode} | #{legislators}"
+
+  #personal_letter = template_letter.gsub('FIRST_NAME', name)
+  #personal_letter.gsub!('LEGISLATORS', legislators)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
 end
